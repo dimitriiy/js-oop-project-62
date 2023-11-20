ee68a98dedab8025882538468bc192d5584824d9
Mazitov, Dmitry <dmitry.mazitov@x5.ru>
2023-11-20 22:08:03 +0300

    Added number validator

--- src/Validator.js
+++ src/Validator.js
@@ -1,4 +1,20 @@
+import { v } from "npm-check-updates/build/src/lib/version-util.js";
+
 class BaseValidator {
+    constructor(isRequired) {
+        this.isRequired = isRequired;
+        this.validators = [];
+    }
+
+    isEmptyValue(value) {
+        if (!this.isRequired && (value == null || value === "")) return true;
+
+        return false;
+    }
+
+    addValidators(fn) {
+        this.validators.push(fn);
+    }
     required() {
         this.isRequired = true;
         return this;
@@ -6,12 +22,12 @@ class BaseValidator {
 }
 class StringValidator extends BaseValidator {
     constructor(isRequired) {
-        super();
-        this.isRequired = isRequired;
+        super(isRequired);
+
         this.validators = [];
     }
     isValid(value) {
-        if (!this.isRequired && (value == null || value === "")) return true;
+        if (this.isEmptyValue(value)) return true;
 
         if (this.validators.length && value) {
             return this.validators.every((fn) => fn(value));
@@ -25,17 +41,47 @@ class StringValidator extends BaseValidator {
         };
     }
     contains(value) {
-        this.validators.push(this._contains(value));
+        this.addValidators(this._contains(value));
         return this;
     }
 }
-class NumberValidator extends BaseValidator {}
+class NumberValidator extends BaseValidator {
+    _isPositive(value) {
+        return value > 0;
+    }
+
+    _isInRange(start, end) {
+        return (value) => start <= value && value <= end;
+    }
+
+    positive() {
+        this.addValidators(this._isPositive);
+
+        return this;
+    }
+
+    range(start, end) {
+        this.addValidators(this._isInRange(start, end));
+    }
+
+    isValid(value) {
+        if (this.isEmptyValue(value)) return true;
+
+        if (this.validators.length) {
+            return this.validators.every((fn) => fn(value));
+        }
+
+        return typeof value === "number";
+    }
+}
 
 export class Validator extends BaseValidator {
     string() {
         return new StringValidator(this.isRequired);
     }
-    number() {}
+    number() {
+        return new NumberValidator(this.isRequired);
+    }
 
     shape() {}
 }
--- src/__tests__/Validator.test.js
+++ src/__tests__/Validator.test.js
@@ -35,4 +35,51 @@ describe("Test Validator", () => {
             expect(schema.isValid("what does the fox say")).toBeFalsy();
         });
     });
+
+    describe("Test number validation", () => {
+        it("Test null", () => {
+            const v = new Validator();
+
+            const schema = v.number();
+
+            expect(schema.isValid(null)).toBeTruthy(); // true
+        });
+
+        it("Test required", () => {
+            const v = new Validator();
+
+            const schema = v.number();
+
+            schema.isValid(null); // true
+
+            schema.required();
+
+            expect(schema.isValid(null)).toBeFalsy(); // false
+            expect(schema.isValid(7)).toBeTruthy(); // true
+        });
+
+        it("Test range", () => {
+            const v = new Validator();
+
+            const schema = v.number();
+
+            schema.range(-10, 10);
+
+            expect(schema.isValid(10)).toBeTruthy();
+            expect(schema.isValid(-2020)).toBeFalsy();
+            expect(schema.isValid(11)).toBeFalsy();
+        });
+
+        it("Test positive", () => {
+            const v = new Validator();
+
+            const schema = v.number();
+
+            schema.positive();
+
+            expect(schema.isValid(10)).toBeTruthy();
+            expect(schema.isValid(-2020)).toBeFalsy();
+            expect(schema.isValid(-1)).toBeFalsy();
+        });
+    });
 });
